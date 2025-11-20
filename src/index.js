5aad3c5977456bf347bde081e39458a379b30d33
unknown <pongsapat.mainmail@gmail.com>
2025-11-20 21:34:47 +0700

    add pwa

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { CartProvider } from './context/CartContext';
 import { AuthProvider } from './context/AuthContext';
 import { ViewHistoryProvider } from './context/ViewHistoryContext';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // เพิ่มตรงนี้
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,3 +19,6 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 );
+
+// ลงทะเบียน service worker เพื่อให้เว็บเป็น PWA
+serviceWorkerRegistration.register();
